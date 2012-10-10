abcefcb7c8d3089c4143ba7ccb1c3365b103c186
David Padilla <david@crowdint.com>
2012-10-09 23:58:11 -0500

    Amazon code to validate signatures

--- app/controllers/fundraiser/ipns_controller.rb
+++ app/controllers/fundraiser/ipns_controller.rb
@@ -1,9 +1,18 @@
 module Fundraiser
   class IpnsController < ApplicationController
-    def new
-    end
-
     def create
+      aws_access_key = ENV['FPS_AWS_ACCESS_KEY_ID']
+      aws_secret_key = ENV['FPS_AWS_SECRET_ACCESS_KEY']
+      url_end_point = fundraiser.ipns_url
+
+      utils = Amazon::FPS::SignatureUtilsForOutbound.new(aws_access_key, aws_secret_key);
+
+      if utils.validate_request(:parameters => params, :url_end_point => url_end_point, :http_method => "POST")
+        Rails.logger.info "CRY OF JOY"
+        render :text => "OK"
+      else
+        Rails.logger.info "CRY"
+      end
     end
   end
 end
--- lib/fundraiser.rb
+++ lib/fundraiser.rb
@@ -2,6 +2,7 @@ require "fundraiser/engine"
 require "devise"
 require "haml-rails"
 require "persistent_settings"
+require "amazon/fps/signature_utils_for_outbound"
 
 module Fundraiser
 end
