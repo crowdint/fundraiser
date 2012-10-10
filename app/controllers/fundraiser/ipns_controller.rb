81bea0015ea4e28e4984260bf1a9fef8dd9c3bb3
David Padilla <david@crowdint.com>
2012-10-10 00:17:21 -0500

    Fixes the Heroku SSL Issue?

--- app/controllers/fundraiser/ipns_controller.rb
+++ app/controllers/fundraiser/ipns_controller.rb
@@ -5,7 +5,7 @@ module Fundraiser
       aws_secret_key = ENV['FPS_AWS_SECRET_ACCESS_KEY']
       url_end_point = fundraiser.ipns_url
 
-      utils = Amazon::FPS::SignatureUtilsForOutbound.new(aws_access_key, aws_secret_key);
+      utils = Amazon::FPS::SignatureUtilsForOutbound.new(aws_access_key, aws_secret_key)
 
       if utils.validate_request(:parameters => params, :url_end_point => url_end_point, :http_method => "POST")
         Rails.logger.info "CRY OF JOY"
--- lib/fundraiser.rb
+++ lib/fundraiser.rb
@@ -4,5 +4,14 @@ require "haml-rails"
 require "persistent_settings"
 require "amazon/fps/signature_utils_for_outbound"
 
+#TODO: This line is here due a Heroku issue:
+#
+#  http://stackoverflow.com/questions/11703679/opensslsslsslerror-on-heroku
+#
+#  No idea how it will work on other platforms
+#
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+
 module Fundraiser
 end
