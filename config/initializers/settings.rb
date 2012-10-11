5b7f4dfc65d29135fac961426a27708607d88a03
David Padilla <david@crowdint.com>
2012-10-11 17:20:31 -0500

    Can use amazon on Production mode

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,5 +1,6 @@
 Fundraiser::Settings.load_from_persistance
 
 if Fundraiser::Settings.ready?
-  Fundraiser::Settings.amazon_checkout_url = "https://authorize.payments-sandbox.amazon.com/pba/paypipeline"
+  Fundraiser::Settings.
+      amazon_checkout_url = ENV['AMAZON_FPS_URL'] || "https://authorize.payments-sandbox.amazon.com/pba/paypipeline"
 end
