3355763232ff6da3622ab622ba07180e5023d909
David Padilla <david@crowdint.com>
2012-10-12 12:25:37 -0500

    After paying with amazon, the return url should be the thank you page

--- app/helpers/fundraiser/application_helper.rb
+++ app/helpers/fundraiser/application_helper.rb
@@ -10,7 +10,7 @@ module Fundraiser
 
     def simplepay_button(reward)
       amount = "USD #{reward.minimum_pledge}"
-      Amazon::FPS::Widget.widget amount, reward.title, reward.id, fundraiser.rewards_url, fundraiser.ipns_url
+      Amazon::FPS::Widget.widget amount, reward.title, reward.id, fundraiser.thanks_url, fundraiser.ipns_url
     end
   end
 end
