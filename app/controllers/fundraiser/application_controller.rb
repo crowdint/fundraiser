f5d20de088142dea35c866ef8d2757696519bc40
David Padilla <david@crowdint.com>
2012-10-11 21:04:09 -0500

    Use method_missing for authenticate_user!

--- app/controllers/fundraiser/application_controller.rb
+++ app/controllers/fundraiser/application_controller.rb
@@ -1,5 +1,11 @@
 module Fundraiser
   class ApplicationController < ActionController::Base
+    def method_missing(method_name)
+      if method_name == :authenticate_user!
+        Rails.logger.warn("authenticate_user! in Crowdblog::ApplicationController should be overriden")
+      end
+    end
+
     protected
     def sanity_check
       redirect_to fundraiser.edit_manage_settings_path unless Fundraiser::Settings.try(:funding_goal)
--- app/controllers/fundraiser/manage/base_controller.rb
+++ app/controllers/fundraiser/manage/base_controller.rb
@@ -1,4 +1,6 @@
 class Fundraiser::Manage::BaseController < Fundraiser::ApplicationController
+  before_filter :authenticate_user!
+
   def after_sign_in_path_for(resource)
     fundraiser.root_path
   end
